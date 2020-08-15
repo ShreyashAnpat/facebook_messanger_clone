adf3fcd32eb1ee9e045d49ad1290256733b80441
anpatshreyash <anpatshreyash@gmail.com>
2020-08-16 01:57:51 +0530

    Updates

---
+++ precache-manifest.46985f002361fe6ef39490f8cbc5ab72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cfbefa0511700316ad28f04cfa8a676",
+    "url": "/facbook_messanger_clone/index.html"
+  },
+  {
+    "revision": "0e11bc83c25f7f796ebb",
+    "url": "/facbook_messanger_clone/static/css/main.0c9043d4.chunk.css"
+  },
+  {
+    "revision": "4afe712e63f6b4733f8b",
+    "url": "/facbook_messanger_clone/static/js/2.5180b655.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facbook_messanger_clone/static/js/2.5180b655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e11bc83c25f7f796ebb",
+    "url": "/facbook_messanger_clone/static/js/main.77892be6.chunk.js"
+  },
+  {
+    "revision": "ab8e0dfb2d4d0c875ac1",
+    "url": "/facbook_messanger_clone/static/js/runtime-main.ca104828.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/facbook_messanger_clone/precache-manifest.46985f002361fe6ef39490f8cbc5ab72.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facbook_messanger_clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
